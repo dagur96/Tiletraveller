9ca33af9b9acc861d0e90cb497032a967cc26261
Dagur Hilmarsson <Dagur@Dagurs-MacBook-Pro.local>
2018-09-18 10:02:23 +0000

    added algorithm

---
+++ Tile.py
@@ -0,0 +1,16 @@
+# The player starts at 1,1
+# The player is supposed to got to square 3,1
+# The player can only move to specific destination on each square
+# Good luck
+
+#1,1 -> N Start
+#1,2 -> N, S, or E
+#1,3 -> S or E
+#2,1 -> N
+#2,2 -> S or W
+#2,3 -> W or E
+#3,1 -> N  Victory Royale
+#3,2 -> N or S
+#3,3 -> W or S
+
+
